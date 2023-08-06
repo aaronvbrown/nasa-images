581ef4243fc252ce08ffbe59c722f89574cead44
Aaron Brown <worldofaaron@hotmail.com>
2023-08-06 12:44:07 -0700

    initial mvp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import APOD from './apod.js';
+
+//Business Logic
+
+function getAPOD() {
+  APOD.getPic()
+    .then(function (response) {
+      if (response.url) {
+        appendPic(response);
+      } else {
+        printError(response);
+      }
+    })
+}
+
+//UI Logic
+
+function appendPic(response) {
+  let picDiv = document.getElementById('spacepic');
+  let img = document.createElement('img');
+  img.src = response.url;
+  picDiv.innerText = response.title;
+  picDiv.append(img);
+
+}
+
+function printError(error) {
+  document.getElementById("picDiv").append(error.message);
+}
+
+window.addEventListener("load", getAPOD);
